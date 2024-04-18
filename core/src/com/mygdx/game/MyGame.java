48e01d7ab6b0fb3ac22d6f7aecfd7ccaff3824b1
SenPie <smbat@duck.com>
2024-04-18 19:19:32 +0200

    Added variation and collision check for droplets.

--- core/src/com/mygdx/game/MyGame.java
+++ core/src/com/mygdx/game/MyGame.java
@@ -5,27 +5,38 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.utils.Timer;
 
 
 public class MyGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	OrthographicCamera camera;
+	BitmapFont font;
 
 	Texture dropPng;
 	Texture bucketPng;
 
-	float bucketX = 0;
+	Rectangle bucketRect;
+	Array<Rectangle> dropletRects;
+	Array<Float> dropletDelays;
+
 	float bucketSpeed = 600; // px per second
-
 	float prevTime;
+	int score = 0;
 
-	Array<Float> dropletPositions;
-	float dropletY = 300;
+	float spawnRate = 1.5f;
+
+	/*
+		1. Get rid off the droplets when they leave screen.
+		2. Make it more interesting.
+	 */
 
 	@Override
 	public void create () {
@@ -33,15 +44,22 @@ public class MyGame extends ApplicationAdapter {
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 480);
 
+		font = new BitmapFont();
+
 		bucketPng = new Texture("bucket.png");
 		dropPng = new Texture("drop.png");
 
-		bucketX = (Gdx.graphics.getWidth() / 2.f) - (bucketPng.getWidth() / 2.0f);
+		bucketRect = new Rectangle(
+			(Gdx.graphics.getWidth() / 2.f) - (bucketPng.getWidth() / 2.0f), 20,
+			bucketPng.getWidth(), bucketPng.getHeight()
+		);
+
 		prevTime = TimeUtils.nanoTime();
 
-		dropletPositions = new Array<>();
+		dropletRects = new Array<>();
+		dropletDelays = new Array<>();
 		for (int i = 0; i < 8; i++) {
-			dropletPositions.add(MathUtils.random(0.0f, 800.0f - dropPng.getWidth()));
+			createDroplet();
 		}
 	}
 
@@ -53,22 +71,62 @@ public class MyGame extends ApplicationAdapter {
 		ScreenUtils.clear(0.1f, 0.1f, 0.5f, 1);
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(bucketPng, bucketX, 20);
-		for (int i = 0; i < dropletPositions.size; i++) {
-			batch.draw(dropPng, dropletPositions.get(i), dropletY);
+		batch.draw(bucketPng, bucketRect.x, bucketRect.y);
+		for (Rectangle dropletRect : dropletRects) {
+			batch.draw(dropPng, dropletRect.x, dropletRect.y);
 		}
+
+		font.draw(
+				batch,
+				"Score: " + score,
+				Gdx.graphics.getWidth() / 50.0f, Gdx.graphics.getHeight() - 20
+		);
 		batch.end();
 
 		// game logic
 		boolean left = Gdx.input.isKeyPressed(Input.Keys.LEFT);
 		boolean right = Gdx.input.isKeyPressed(Input.Keys.RIGHT);
 		if (left) {
-			bucketX = bucketX - (bucketSpeed * Gdx.graphics.getDeltaTime()); // how much time it took to render last frame
+			bucketRect.x -= (bucketSpeed * Gdx.graphics.getDeltaTime()); // how much time it took to render last frame
 		} else if (right) {
-			bucketX = bucketX + (bucketSpeed * Gdx.graphics.getDeltaTime());
+			bucketRect.x += (bucketSpeed * Gdx.graphics.getDeltaTime());
 		}
 
-		dropletY--;
+		for (int i = 0; i < dropletRects.size; i++) {
+			float delay = dropletDelays.get(i);
+			if (delay < 0) {
+				Rectangle dropletRect = dropletRects.get(i);
+				dropletRect.y -= 100 * Gdx.graphics.getDeltaTime();
+
+				if (dropletRect.overlaps(bucketRect)) {
+					dropletRects.removeValue(dropletRect, true);
+					dropletDelays.removeIndex(i);
+					score++;
+				}
+			} else {
+				delay -= Gdx.graphics.getDeltaTime(); // seconds 0.016 0.0123
+				dropletDelays.set(i, delay);
+			}
+		}
+
+		if (spawnRate < 0) {
+			createDroplet();
+			spawnRate = 1.5f;
+		} else {
+			spawnRate -= Gdx.graphics.getDeltaTime();
+		}
+	}
+
+	private void createDroplet () {
+		float delay = MathUtils.random(0.5f, 2.f);
+		dropletDelays.add(delay);
+		Rectangle dropletRect = new Rectangle(
+				MathUtils.random(0.0f, 800.0f - dropPng.getWidth()),
+				Gdx.graphics.getHeight() + MathUtils.random(20, 200),
+				dropPng.getWidth(),
+				dropPng.getHeight()
+		);
+		dropletRects.add(dropletRect);
 	}
 
 	@Override
